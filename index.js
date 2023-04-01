44e665e7cad412102c752796b795abfe4a11ebe7
Rabbia Khan <60133658+rabbiakhan@users.noreply.github.com>
2023-04-01 22:00:07 +0500

    update index.js with all pages

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
-// function changeText() {
-//   //   Event.preventDefault();
-//   let text = document.querySelector("#para");
-//   let input = document.querySelector("#input-value");
-//   text.innerHTML = input.value;
-// }
-// let inputValue = document.querySelector("#search-btn");
-// inputValue.addEventListener("submit", changeText);
+function changeText() {
+  event.preventDefault();
+  let text = document.querySelector("#para-text");
+  let input = document.querySelector("#input-value");
+  text.innerHTML = input.value;
+  text = document.querySelector("#para-text").style.fontSize = "40px";
+  text = document.querySelector("#para-text").style.textAlign = "center";
+  text = document.querySelector("#para-text").style.textTransform = "uppercase";
+}
+let inputValue = document.querySelector("#search-form");
+inputValue.addEventListener("submit", changeText);
 
-// function updateHeading(newHeading) {
-//   let heading = document.querySelector("h1");
-//   heading.innerHTML = newHeading;
-//   heading = document.querySelector("h1").style.color = "white";
-//   heading = document.querySelector("h1").style.fontSize = "11px";
-// }
-function foodie() {
-  let name = prompt("Welcome! What is your Name?");
-  let country = prompt("🌎From which country are you?");
+function userInfo() {
+  // event.prevenDefault();
+  let user = prompt("👋Welcome!, What is your name?");
+  let country = prompt("🌎 From which country are you?");
   let food = prompt(
-    "What type of food you want from us? Asian, French, Greek.😇"
+    "What type of food you want to order? Asian, French, Greek.😇"
   );
   food = food.toLowerCase().trim();
-  if (food === "asian" || food === "french" || food === "greek") {
-    let h1 = document.querySelector("#para");
-    h1.innerHTML = `Hi👋 ${name}, Thank's for connected from ${country}, Happy Exploring ${food} food😍`;
-    // updateHeading(
-    //   `Hi👋 ${name}, Thank's for connected from ${country}, Happy Exploring ${food} food😍`
-    // );
+  if (food === "asian" || food == "french" || food == "greek") {
+    alert(
+      `Hi👋 ${user}, Thank's for connecting from ${country}, Happy exploring ${food} food.😍`
+    );
   } else {
     alert(
-      `Ahhh ${name} your ${food} food is out of box😕, Stay connected for taste different🤤`
+      `Ahhh ${user} your ${food} food is out of list😕, Stay connected for taste different🤤`
     );
-    // updateHeading(
-    //   `Ahhh ${name} your ${food} food is out of box😕, Stay connected for taste different🤤`
-    // );
   }
 }
-let login = document.querySelector("#sign-in");
-login.addEventListener("click", foodie);
+let signIn = document.querySelector("#sign-in");
+signIn.addEventListener("click", userInfo);
